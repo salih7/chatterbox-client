ba48f3296573b3f1b7ecd0aba264b7cfbbe0f216
Hack Reactor Student <student@hackreactor.com>
2017-04-07 15:02:21 -0700

    Dropped in example starter code

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,1 +1,25 @@
 // YOUR CODE HERE:
+// Server: http://parse.sfm8.hackreactor.com/
+
+
+// -------------- Example Requests and Messages --------------------------
+// $.ajax({
+//   // This is the url you should use to communicate with the parse API server.
+//   url: 'http://parse.CAMPUS.hackreactor.com/chatterbox/classes/messages',
+//   type: 'POST',
+//   data: JSON.stringify(message),
+//   contentType: 'application/json',
+//   success: function (data) {
+//     console.log('chatterbox: Message sent');
+//   },
+//   error: function (data) {
+//     // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+//     console.error('chatterbox: Failed to send message', data);
+//   }
+// });
+
+// var message = {
+//   username: 'shawndrost',
+//   text: 'trololo',
+//   roomname: '4chan'
+// };
