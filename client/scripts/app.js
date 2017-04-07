c0daf127b2f0c33837ea6bc14091e7664e1c0a33
Hack Reactor Student <student@hackreactor.com>
2017-04-07 15:34:40 -0700

    Create an app object and perform GET request

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -23,3 +23,33 @@
 //   text: 'trololo',
 //   roomname: '4chan'
 // };
+
+class App {
+  constructor() {
+    this.server = 'http://parse.sfm8.hackreactor.com/';
+  }
+  init() {
+
+  }
+  send() {
+
+  }
+  fetch() {
+    $.ajax({
+      // This is the url you should use to communicate with the parse API server.
+      url: this.server,
+      type: 'GET',
+      data: JSON.stringify(message),
+      contentType: 'application/json',
+      success: function (data) {
+        console.log('chatterbox: Message sent');
+      },
+      error: function (data) {
+        // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+        console.error('chatterbox: Failed to send message', data);
+      }
+    });
+  }
+}
+
+var app = new App;
