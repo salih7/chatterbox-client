7fffd03b57384c5ad87e189eb93ae773c431483e
Hack Reactor Student <student@hackreactor.com>
2017-04-07 16:17:44 -0700

    Add message field and room fields to page

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -28,12 +28,29 @@ class App {
   constructor() {
     this.server = 'http://parse.sfm8.hackreactor.com/';
   }
+
   init() {
-
+    this.fetch();
+    this.send();
   }
-  send() {
 
+  send(message) {
+    $.ajax({
+      // This is the url you should use to communicate with the parse API server.
+      url: this.server,
+      type: 'POST',
+      data: JSON.stringify(message),
+      contentType: 'application/json',
+      success: function (data) {
+        console.log('chatterbox: Message sent');
+      },
+      error: function (data) {
+        // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+        console.error('chatterbox: Failed to send message', data);
+      }
+    });
   }
+
   fetch() {
     $.ajax({
       // This is the url you should use to communicate with the parse API server.
